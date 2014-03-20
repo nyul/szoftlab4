9036f9c37956d7c61259fae663a6c53bbf1cb07a
Szabolcs Bana <nyul@users.noreply.github.com>
2014-03-20 17:59:33 +0100

    Fellowship és PlayingArea skeleton implementáció

--- code/src/Fellowship.java
+++ code/src/Fellowship.java
@@ -1,38 +1,32 @@
-import java.util.ArrayList;
 
+public class Fellowship {	
+	
+	public Fellowship() {
+		System.out.println("Fellowship()");
+	}
+	
+	public void produceAllEnemy(){
+		System.out.println("produceAllEnemy()");
+	}
+	
+	public void startWave(){
+		System.out.println("startWave()");
+	}
+	
+	public void getNumber(){
+		System.out.println("getNumber()");
+	}
+	
+	public void killEnemy(){
+		System.out.println("killEnemy(enemy)");
+	}
+	
+	public void getActiveEnemies(){
+		System.out.println("getActiveEnemies()");
+	}
 
-public class Fellowship {
-	private ArrayList<Wave> wave;
-	private int number;
-	private int deltaTime;
-	
-	public Fellowship(int n, int time) {
-		wave = new ArrayList<Wave>();
-		number = n;
-		deltaTime = time;
-	}
-	
-	public void addWave(Wave w) {
-		if(isAddWave()) {
-			wave.add(w);
-		}
-	}
-	
-	public boolean isAddWave() {
-		if(wave.size() <= number) return true;
-		else return false;
-	}
-	
-	public void removeWave(Wave w) {
-		wave.remove(w);
-	}
-	
-	public int getNumber() {
-		return number;
-	}
-	
-	public int getDeltaTime() {
-		return deltaTime;
-	}
-	
+	public void moveAllActiveEnemy(){
+		System.out.println("moveAllActiveEnemy");
+	}	
+
 }
--- code/src/PlayingArea.java
+++ code/src/PlayingArea.java
@@ -1,103 +1,30 @@
-import java.util.ArrayList;
-
 
 public class PlayingArea {
-	private ArrayList<Tile> tile;
-	private ArrayList<Source> source;
-	private ArrayList<Road> roadOne;
-	private ArrayList<Road> roadTwo;
-	private ArrayList<Tower> tower;
-	private ArrayList<Obstacle> obstacle;
-	private Mountain mountain;
 	
-	public PlayingArea() {
-		tile = new ArrayList<Tile>();
-		source = new ArrayList<Source>();
-		roadOne = new ArrayList<Road>();
-		roadTwo = new ArrayList<Road>();
-		tower = new ArrayList<Tower>();
-		obstacle = new ArrayList<Obstacle>();
-	}
+	public PlayingArea() {}
 	
 	public void initialize() {
+		System.out.println("initialize()");
+	}
+	
+	public void getObstacleList() {
+		System.out.println("getObstacleList()");
+	}
+	
+	public void getTowerList() {
+		System.out.println("getTowerList()");
+	}
+	
+	public void getMountain() {
+		System.out.println("getMountain()");
+	}
 		
-		for(int i = 0; i < 5; i++) {
-			for(int j = 0; j < 5; j++) {
-				tile.add(new Tile('0', new Position(i, j)));
-			}
-		}
-		
-		source.add(new Source('2', new Position(0, 4), new Position(0, 3)));
-		source.add(new Source('2', new Position(3, 4), new Position(3, 3)));
-		
-		mountain = new Mountain('2', new Position(2, 0), null);
-		
-		roadOne.add(new Road('1', new Position(0, 3), new Position(0, 2)));
-		roadOne.add(new Road('1', new Position(0, 2), new Position(0, 1)));
-		roadOne.add(new Road('1', new Position(0, 1), new Position(1, 1)));
-		roadOne.add(new Road('1', new Position(1, 1), new Position(1, 0)));
-		roadOne.add(new Road('1', new Position(1, 0), new Position(2, 0)));
-		
-		roadTwo.add(new Road('1', new Position(3, 3), new Position(3, 2)));
-		roadTwo.add(new Road('1', new Position(3, 2), new Position(3, 1)));
-		roadTwo.add(new Road('1', new Position(3, 1), new Position(3, 0)));
-		roadTwo.add(new Road('1', new Position(3, 0), new Position(2, 0)));
+	public void addTower() {
+		System.out.println("addTower(tower)");
 	}
 	
-	public boolean isSource(Tile t) {
-		for(int i = 0; i < source.size(); i++) {
-			if(source.get(i).getPos().equals(source.get(i).getPos(), t.getPos())) return true;
-		}
-		return false;
+	public void addObstacle() {
+		System.out.println("addObstacle(obstacle)");
 	}
 	
-	public boolean isMountain(Tile t) {
-		if(mountain.getPos().equals(mountain.getPos(), t.getPos())) return true;
-		return false;
-	}
-	
-	public boolean isRoadOne(Tile t) {
-		for(int i = 0; i < roadOne.size(); i++) {
-			if(roadOne.get(i).getPos().equals(roadOne.get(i).getPos(), t.getPos())) return true;
-		}
-		return false;
-	}
-	
-	public boolean isRoadTwo(Tile t) {
-		for(int i = 0; i < roadTwo.size(); i++) {
-			if(roadTwo.get(i).getPos().equals(roadTwo.get(i).getPos(), t.getPos())) return true;
-		}
-		return false;
-	}
-	
-	public void addTower(Tower t) {
-		tower.add(t);
-	}
-	
-	public void addObstacle(Obstacle o) {
-		obstacle.add(o);
-	}
-	
-	public ArrayList<Tile> getTileList() {
-		return tile;
-	}
-	
-	public Tile getTile(Position p) {
-		for(int i = 0; i < tile.size(); i++) {
-			if(tile.get(i).getPos() == p) return tile.get(i);
-		}
-		return null;
-	}
-	
-	public ArrayList<Road> getRoadOneList() {
-		return roadOne;
-	}
-	
-	public ArrayList<Road> getRoadTwoList() {
-		return roadTwo;
-	}
-	
-	public ArrayList<Source> getSourceList() {
-		return source;
-	}
 }
