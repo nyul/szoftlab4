241406c32020bc845f574d7068119e99d880ed80
vcsontos <csontos.valentin@gmail.com>
2014-03-20 18:16:06 +0100

    Main modositasa: menu elkeszitese

--- code/src/Engine.java
+++ code/src/Engine.java
@@ -1,41 +1,31 @@
 
 public class Engine extends Thread{
-	private Player player;
-	private Fellowship fellowship;
-	
-	public Engine(Player p, Fellowship f) {
-		player = p;
-		fellowship = f;
-		//human = new Human(100, 10, new Position(0, 4), new Position(0, 3));
-	}
  	
+	public void attackHandler() {
+		System.out.println("attackHandler()");
+	}
+	
 	public void stepHandler() {
 		
 	}
-	public void towerHandler() {
+	
+	public void takeToArea() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void defeat() {
+		
+	}
+	
+	public void victory() {
 		
 	}
 	
 	public Player getPlayer() {
-		return player;
-	}
-	
-	public Fellowship getFellowShip() {
-		return fellowship;
-	}
-	
-	public void run() {
-		while(true) {
-			stepHandler();
-			towerHandler();
-			//human.move();
-			System.out.println("Hello");
-		
-			try {
-				Thread.sleep(100);
-			} catch(Exception ex) {
-				ex.printStackTrace();
-			}
-		}
+		return new Player(10);
 	}
 }
--- code/src/Main.java
+++ code/src/Main.java
@@ -1,20 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
 	
-	public static void main(String[] args) {  // sorrend fontos
-		Engine engine = new Engine(new Player(100), new Fellowship(2, 6));
-		engine.getPlayer().startGame();
+	private static int number;
+	
+	public void menu() {
+		switch(number) {
+		case 0:
+			indítás();
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		case 11:
+			break;
+		case 12:
+			break;
+		}
+	}
+	
+	public void indítás() {
 		
-		Tower t = engine.getPlayer().buildTower(new Tile('0', new Position(1, 2)));
-		t.setType('2');
-		engine.getPlayer().getPlayingArea().addTower(t);
-		
-		engine.getPlayer().buyMagicRock(t, 0);
-		
-		Obstacle o = engine.getPlayer().buildObstacle(new Tile('1', new Position(0, 2)));
-		o.setType('2');
-		engine.getPlayer().getPlayingArea().addObstacle(o);
-		
-		engine.run();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Main main = new Main();
+		System.out.println("Use-case menu");
+		System.out.println("1.Inditas");
+		System.out.println("2.Torony epites");
+		System.out.println("3.Akadaly epites");
+		System.out.println("4.Torony fejlesztes");
+		System.out.println("5.Akadaly fejlesztes");
+		System.out.println("6.Ellenseg palyara helyezese");
+		System.out.println("7.Ellenseg leptetese");
+		System.out.println("8.Ellenseg lassitasa");
+		System.out.println("9.Vereseg");
+		System.out.println("10.Tuzeles");
+		System.out.println("11.Ellenseg elpusztulasa");
+		System.out.println("12.Gyozelem");
+		System.out.println("0.Kilepes");
+		System.out.println("");
+		System.out.println("Choose a use-case!");
+		InputStreamReader read = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(read);
+        number = Integer.parseInt(in.readLine());
+		main.menu();
 	}
 }
