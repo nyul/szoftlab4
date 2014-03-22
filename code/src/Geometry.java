a51476de8de2ecfd771f94c6f549699c16e28b10
Szabolcs Bana <nyul@users.noreply.github.com>
2014-03-22 18:47:50 +0100

    Manuális merge toto szkeleton -> master -Toto szkeleton commitjai mergelve a master branchbe -Toto diagramjai mergelve a master branchbe

---
+++ code/src/Geometry.java
@@ -0,0 +1,11 @@
+
+public class Geometry {
+	
+	public Geometry() {
+		System.out.println("Geometry constructor");
+	}
+
+	public void isInRange(){
+		System.out.println("isInRange(enemy, tower)");
+	}	
+}
--- code/src/Position.java
+++ code/src/Position.java
@@ -1,35 +1,14 @@
 
 public class Position {
-	private int x;
-	private int y;
 	
-	Position(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Position() {
+		System.out.println("Position constructor");
 	}
-	
-	public int getX() {
-		return x;
+
+	public void getX(){		
+		System.out.println("getX()");					
 	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public void setX(int number) {
-		x = number;
-	}
-	
-	public void setY(int number) {
-		y = number;
-	}
-	
-	public boolean equals(Position p1, Position p2) {
-		if(p1.x == p2.x && p1.y == p2.y) return true;
-		else return false;
-	}
-	
-	public double distance(Position p1, Position p2) {
-		return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+	public void getY(){		
+		System.out.println("getY()");							
 	}
 }
