9850401bcfe72ce9871c046fd12511a2f5197111
Szabolcs Bana <nyul@users.noreply.github.com>
2014-03-23 23:16:05 +0100

    hianyzo resz potolasa

--- code/src/Road.java
+++ code/src/Road.java
@@ -1,9 +1,23 @@
 
 public class Road extends Tile{
+
+	Road nextRoad;
 	
+	public Road() {
+		nextRoad = null;
+	}
+	
+	public void createObject() {
+		nextRoad = new Road();
+	}
+	
+	/**
+	 * Atadja a hivonak az ut kovetkezo poziciojat (ahova lepnie kell)
+	 * @param e A lepni kivano enemy referenciaja
+	 */
 	public void requestDestination(Enemy e) {
 		Writer.entry();
-		
+		e.setRoad(nextRoad);
 		Writer.asynchronexit();
 	}
 }
