4bdb7c883fb3dc0b4e838ccd038d53f867c71289
vcsontos <csontos.valentin@gmail.com>
2014-03-23 11:15:21 +0100

    Tower valtoztatva

--- code/src/Tower.java
+++ code/src/Tower.java
@@ -45,15 +45,112 @@ public class Tower extends Tile implements Defense{
 		player.chooseUpgrade(choosable);
 		Writer.asynchronexit();		
 	}
+
+	/**
+	 * Enemy konstruktor
+	 */
 	
-	/**a
+
+	public void upgrade(MagicRock magicRock){
+		Writer.entry();
+		Writer.asynchronexit();
+
+	/**
 	 * Enemy konstruktor
 	 */
 	
 	public void upgrade(MagicRock magicRock){
 		Writer.entry();
 		Writer.asynchronexit();
+	
+	/**
+	 * Setterek és Getterek
+	 */
+	
+	public int getSootPeriod(){
+		Writer.entry();
+		Writer.synchronexit();
+		return shootPeriod;
 	}
+	
+	public void setSootPeriod(int sp){
+		Writer.entry();
+		shootPeriod = sp;
+		Writer.asynchronexit();		
+	}
+	
+	public int getRange(){
+		Writer.entry();
+		Writer.synchronexit();
+		return range;
+	}
+
+	public void setRange(int r){
+		Writer.entry();
+		range = r;
+		Writer.asynchronexit();		
+	}
+	
+	public int getDamagePowerHuman(){
+		Writer.entry();
+		Writer.synchronexit();
+		return damagePowerHuman;
+	}
+
+	public void setDamagePowerHuman(int p){
+		Writer.entry();
+		damagePowerHuman = p;
+		Writer.asynchronexit();
+	}
+	
+	public int getDamagePowerHobbit(){
+		Writer.entry();
+		Writer.synchronexit();
+		return damagePowerHobbit;
+	}
+
+	public void setDamagePowerHobbit(int p){
+		Writer.entry();
+		damagePowerHobbit = p;
+		Writer.asynchronexit();
+	}
+	
+	public int getDamagePowerDwarf(){
+		Writer.entry();
+		Writer.synchronexit();
+		return damagePowerDwarf;
+	}
+
+	public void setDamagePowerDwarf(int p){
+		Writer.entry();
+		damagePowerDwarf = p;
+		Writer.asynchronexit();
+	}
+	
+	public int getDamagePowerElf(){
+		Writer.entry();
+		Writer.synchronexit();
+		return damagePowerElf;
+	}
+
+	public void setDamagePowerElf(int p){
+		Writer.entry();
+		damagePowerElf = p;
+		Writer.asynchronexit();
+	}
+	
+	public int getMagicRockNumber(){
+		Writer.entry();
+		Writer.synchronexit();
+		return magicRockNumber;
+	}
+	
+	public void setMagicRockNumber(int m){
+		Writer.entry();
+		magicRockNumber = m;
+		Writer.asynchronexit();		
+	}
+
 	
 	/**
 	 * Setterek Ã©s Getterek
@@ -142,6 +239,5 @@ public class Tower extends Tile implements Defense{
 		magicRockNumber = m;
 		Writer.asynchronexit();		
 	}
-
 }
 
