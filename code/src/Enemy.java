a5d1e01ab0a91dd7996ba9ee7a94954dce7fa836
Szabolcs Bana <nyul@users.noreply.github.com>
2014-03-23 23:11:49 +0100

    Update Enemy.java
    ekezetek javitasa, hianyzo dolgok potolasa

--- code/src/Enemy.java
+++ code/src/Enemy.java
@@ -1,50 +1,90 @@
 
 public abstract class Enemy {
-
-	/**
-	 * A paraméterként kapott pozícióra lépteti az ellenséget.
-	 * 
-	 * @param p A léptetés célpozíciója
-	 */
-
-	public void move() {
-		Writer.entry();	
-		Writer.asynchronexit();
+	
+	Source source;
+	Road road;
+	Obstacle obstacle;
+	
+	public Enemy() {
+		source = null;
+		road = null;
+		obstacle = null;
 	}
 	
-
+	public void createObject() {
+		source = new Source();
+		road = new Road();
+		obstacle = new Obstacle();
+	}
+	
+	/**
+	 * A parameterkent kapott poziciora lepteti az ellenseget.
+	 * 
+	 * @param p A leptetes celpozicioja
+	 */
+	public void move() {
+		Writer.entry();
+		road.requestDestination(this);
+		Writer.asynchronexit();
+	}
 
 	/**
-	 * A paraméterlistán kapott toronytól leolvasott,
-	 * erre az ellenség típusra vonatkozó sebzés értéket
-	 * levonja az aktuális életerõbõl.
+	 * A parameterlistan kapott toronytol leolvasott,
+	 * erre az ellenseg tipusra vonatkozo sebzes erteket
+	 * levonja az aktualis eleterobol.
 	 * 
-	 * @param t A torony amelyik éppen lö erre az ellenségre
+	 * @param t A torony amelyik eppen lo erre az ellensegre
 	 */
-	
 	public abstract void lifePowerReduce(Tower t);
 
+	/**
+	 * Ellenseg forras poticiora helyezese
+	 * @param pause A kesleltetes erteke, ami megadja a palyara lepes sorrendjet
+	 */
 	public void goToSource(int pause){
 		Writer.entry();
+		source.requestDestination(this);
 		Writer.asynchronexit();
 	}
 
-
+	/**
+	 * A varakozas ertekenek novelese (palyara helyezeskor vagy lassitaskor fordul elo)
+	 * @param slowingFactor Az akadaly lassito tenyezoje
+	 */
 	public void increasePause(int slowingFactor){
 		Writer.entry();
 		Writer.asynchronexit();
 	}		
 
-
+	/**
+	 * Az ellenseget sebzi
+	 * @param t
+	 * @return
+	 */
 	public int hit(Tower t){
 		Writer.entry();
 		Writer.synchronexit();
 		return 0;
 	}
 	
+	/**
+	 * Az ellenseg aktivitasat allitja (~kirajzolas)
+	 * @param a Ha true akkor aktiv lesz, ha false akkor passziv
+	 */
 	public void setActivity(boolean a) {
 		Writer.entry();
 		Writer.asynchronexit();
 	}
-
+	
+	/**
+	 * Beallitja az ellenseg poziciojat, amin allnia kell
+	 * @param nextroad A kovetkezo ut, ahova lepnie kell 
+	 */
+	public void setRoad(Road r) {
+		Writer.entry();
+		if(Writer.kerdes("Kovetkezo ut-csempe akadaly-e?")) {
+			obstacle.slowMe(this);
+		}
+		Writer.asynchronexit();
+	}
 }
