c6b4d85578b78094509ee92af5c1c3b84d5351ca
adtoth <adtoth13@gmail.com>
2014-03-24 01:04:32 +0100

    Tower kommentek

--- code/src/Tower.java
+++ code/src/Tower.java
@@ -17,13 +17,21 @@ public class Tower extends Tile implements Defense{
 		choosable = null;
 	}
 	
+	/**
+	 * szukseges objektumok letrehozasa
+	 */
+	
 	public void createObject() {
 		geometry = new Geometry();
 		enemy = new Human();
 		choosable = new ArrayList<MagicRock>();
 	}
 	
-	
+	/**
+	 * A parameterul kapott enemy listara meghivja a tavolsag vizsgalatot, 
+	 * majd az enemy hit metodusat
+	 * ami csokkenti az enemy erejet
+	 */
 	
 	public Enemy attack(ArrayList<Enemy> enemies) {
 		Writer.entry();
@@ -33,22 +41,30 @@ public class Tower extends Tile implements Defense{
 		return enemy;
 	}
 	
-	
+	/*
+	 * (non-Javadoc)
+	 * @see Defense#wantToUpgrade(Player)
+	 */
 	
 	public void wantToUpgrade(Player player){
 		Writer.entry();
 		player.chooseUpgrade(choosable,this);
 		Writer.asynchronexit();		
-	}
-
+	}	
 	
-	
-
+	/*
+	 * (non-Javadoc)
+	 * @see Defense#upgrade(MagicRock)
+	 */
 	public void upgrade(MagicRock magicRock){
 		Writer.entry();
 		Writer.asynchronexit();
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see Tile#setPos(Position)
+	 */
 	public void setPos(Position pos){
 		Writer.entry();
 		Writer.asynchronexit();
