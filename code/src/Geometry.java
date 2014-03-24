1cc796795fa8786e6e8f365157aa9114b3cf5c76
adtoth <adtoth13@gmail.com>
2014-03-24 01:07:53 +0100

    Geometry kommentek

--- code/src/Geometry.java
+++ code/src/Geometry.java
@@ -1,9 +1,18 @@
 
 public class Geometry {
 	
+	/**
+	 * geometry kosntruktor
+	 */
 	public Geometry() {		
 	}
-
+	
+	/**
+	 * 
+	 * @param enemy megkapja a vizsgalando enemyt
+	 * @param tower megkapja a vizsgalando tornyot
+	 * @return visszaadja, hogy az enemy a torony hatotavan belul van-e
+	 */
 	public boolean isInRange(Enemy enemy, Tower tower){
 		Writer.entry();
 		Writer.synchronexit();
