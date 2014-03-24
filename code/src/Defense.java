54ce719fa29807c838882d12c66300a22c2bb666
adtoth <adtoth13@gmail.com>
2014-03-24 01:10:10 +0100

    Defense komment

--- code/src/Defense.java
+++ code/src/Defense.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 public interface Defense {
 
 	Enemy attack(ArrayList<Enemy> e);
+	/**
+	 * 
+	 * @param p a player ezen keresztul szol
+	 * hogy upgradelni akar valamit, es atadja magat
+	 */
 	void wantToUpgrade(Player p);
+	
+	/**
+	 * fejlesztest vegzo fuggveny 
+	 * @param m parameterkent a varazskovet kapja meg
+	 * amivel a a jatekos fejleszt
+	 */
 	void upgrade(MagicRock m);
 }
