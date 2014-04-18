508256ae3c527b17a0dfcf26f9ea44ca115387bf
vcsontos <csontos.valentin@gmail.com>
2014-04-19 00:24:48 +0200

    Hobbit, Human, MagicRock kommentezése

--- code/src/Hobbit.java
+++ code/src/Hobbit.java
@@ -1,4 +1,9 @@
-
+/**
+ * Ez egy hobbit tipusu ellenseg. Az kulonbozteti meg a tobbi ellensegtol, hogy a torony tuzelese soran a sebzes merteke fugg
+ * a torony specialis kepessegetol. Ha az adott toronynak olyan specialis kepessege van, hogy egy hobbit ellenseget jobban sebez, mint
+ *  a tobbi ellenseget es ha hobbit ellensegbol van a legtobb, akkor a palyan levo tornyokra erdemes olyan varazskovet tenni, hogy az 
+ *  jobban sebezze a hobbit ellenseget, ezaltal hatekonyabb vedekezest biztositva.
+ */
 public class Hobbit extends Enemy{
 
 	public Hobbit() {
@@ -9,6 +14,9 @@ public class Hobbit extends Enemy{
 		super(pos);
 	}
 	
+	/**
+	 * Torony tuzelese soran megsebzi a hobbit ellenseget.
+	 */
 	@Override
 	public void lifePowerReduce(Tower t) {
 		this.lifePower = this.lifePower - t.getDamagePowerHobbit();
--- code/src/Human.java
+++ code/src/Human.java
@@ -1,4 +1,9 @@
-
+/**
+ * Ez egy ember tipusu ellenseg. Az kulonbozteti meg a tobbi ellensegtol, hogy a torony tuzelese soran a sebzes merteke fugg
+ * a torony specialis kepessegetol. Ha az adott toronynak olyan specialis kepessege van, hogy egy ember ellenseget jobban sebez, mint
+ *  a tobbi ellenseget es ha ember ellensegbol van a legtobb, akkor a palyan levo tornyokra erdemes olyan varazskovet tenni, hogy az 
+ *  jobban sebezze az emebr ellenseget, ezaltal hatekonyabb vedekezest biztositva.
+ */
 public class Human extends Enemy{
 
 	public Human() {
@@ -9,6 +14,9 @@ public class Human extends Enemy{
 		super(pos);
 	}
 	
+	/**
+	 * Torony tuzelese soran megsebzi az ember ellenseget.
+	 */
 	@Override
 	public void lifePowerReduce(Tower t) {
 		this.lifePower = this.lifePower - t.getDamagePowerHuman();
