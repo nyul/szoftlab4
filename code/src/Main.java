369cd9ce8677ea3e85365691d5b386c75567dde1
adtoth <adtoth13@gmail.com>
2014-04-20 19:39:46 -0700

    Akadály zárójel

--- code/src/Main.java
+++ code/src/Main.java
@@ -33,7 +33,7 @@ public class Main implements Serializable {
 
 	public void loadInputLanguage() throws IOException {
 
-		FileReader fileReader = new FileReader("input10.txt");
+		FileReader fileReader = new FileReader("input04.txt");
 
 		BufferedReader reader = new BufferedReader(fileReader);
 		String line = reader.readLine();
@@ -43,7 +43,7 @@ public class Main implements Serializable {
 				
 				Tower t = engine.getPlayer().getArea().getGeometry().getTiles().get(Integer.parseInt(command[1])).get(Integer.parseInt(command[2])).buildTower(engine.getPlayer());
 				engine.getPlayer().getArea().addTower(t);
-				System.out.println("[" + Integer.parseInt(command[1]) + ":" + t.getClass().getName() + "] has been built on the Tile(Position(" + Integer.parseInt(command[1]) + "," + Integer.parseInt(command[2]) + ")");
+				System.out.println("[" + Integer.parseInt(command[1]) + ":" + t.getClass().getName() + "] has been built on the Tile(Position(" + Integer.parseInt(command[1]) + "," + Integer.parseInt(command[2]) + "))");
 			}
 			else if(command[0].equals("buildObstacle")) {			
 				for(int i = 0; i < engine.getPlayer().getArea().getRoad().size(); i++){
@@ -205,11 +205,7 @@ public class Main implements Serializable {
 						Elf e = new Elf(new Position(Integer.parseInt(word[1]), Integer.parseInt(word[2])));
 						engine.getFellowship().getActive().add(e);
 						engine.getFellowship().setNumber(engine.getFellowship().getNumber()+1);
-					}
-					else if(word[0].equals("Tile")){
-						Tile t = new Tile(new Position(Integer.parseInt(word[1]), Integer.parseInt(word[2])));
-						engine.getPlayer().getArea().getGeometry().addTile(t);
-					}
+					}					
 					row = br.readLine();
 				}
 				br.close();	
