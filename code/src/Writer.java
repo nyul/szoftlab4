735da1bc220ebbe44ecc72eac1c43ed2b33368d0
Szabolcs Bana <banas0021@gmail.com>
2014-04-23 01:37:27 +0200

    writer comment

--- code/src/Writer.java
+++ code/src/Writer.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
-
+/**
+ * Writer osztaly
+ * Egy string listat tartalmaz, ehhez adjuk hozza a teszt kimeneteket, majd a vegen ebbol irjuk ki a kimeneti outputXX.txt fajlba.
+ */
 public class Writer {
 	public static ArrayList<String> writeText;
 }
