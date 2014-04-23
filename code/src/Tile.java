5a71f8f34e8a8705b0fc8c98016425f9f5b380ae
adtoth <adtoth13@gmail.com>
2014-04-22 23:19:56 -0700

    Tile hello remove

--- code/src/Tile.java
+++ code/src/Tile.java
@@ -56,7 +56,6 @@ public class Tile {
 	 * @return
 	 */
 	public Obstacle buildObstacle(Player player) {
-		System.out.println("Hello");
 		if(player.getMagicPower() > Obstacle.getPrice()) {   // van eleg varazsero
 			if(this.type == 2) {   // ures csempe
 				Obstacle obst = new Obstacle(pos); 
