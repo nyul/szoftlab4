847f43cd3507747b36d425309ee2030708d185a9
vcsontos <csontos.valentin@gmail.com>
2014-04-24 14:36:33 +0200

    PlayerDraw

--- code/src/PlayerDraw.java
+++ code/src/PlayerDraw.java
@@ -9,6 +9,12 @@ public class PlayerDraw implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
+		if (o instanceof Player) {
+	         updateMagicPower(o, arg);
+	    }
+	}
+	
+	public void updateMagicPower(Observable o, Object arg) {
 		if (arg instanceof Integer) {
 			magic = (int) arg;
 			Graphics.text.append("Player magicPower has changed: " + magic + "\n");
