e5126bc839ba14e7097b4ea73a2a5eabe275a9b4
adtoth <adtoth13@gmail.com>
2014-05-03 20:29:31 +0200

    .

--- code/src/GeometryDraw.java
+++ code/src/GeometryDraw.java
@@ -33,7 +33,17 @@ public class GeometryDraw implements Observer {
 				for(int i = 0; i < geo.getTiles().size(); i++) {
 					for(int j = 0; j < geo.getTiles().get(i).size(); j++) {
 						if(geo.getTiles().get(i).get(j).getPos().getX() == obst.getPos().getX() && geo.getTiles().get(i).get(j).getPos().getY() == obst.getPos().getY()) {
-							GraphicsArea.tile[i][j].setBackground(new Color(255, 255, 0));
+							BufferedImage img;
+							try {
+								img = ImageIO.read(new File("img/obstacle.png"));
+								ImageIcon  icon = new ImageIcon(img); 
+								JLabel picLabel = new JLabel(icon);								 
+								GraphicsArea.tile[i][j].add(picLabel);
+								GraphicsArea.tile[i][j].validate();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
 					}
 				}
@@ -65,7 +75,17 @@ public class GeometryDraw implements Observer {
 				for(int i = 0; i < geo.getTiles().size(); i++) {
 					for(int j = 0; j < geo.getTiles().get(i).size(); j++) {
 						if(geo.getTiles().get(i).get(j).getPos().getX() == source.getPos().getX() && geo.getTiles().get(i).get(j).getPos().getY() == source.getPos().getY()) {
-							GraphicsArea.tile[i][j].setBackground(new Color(0, 100, 255));
+							BufferedImage img;
+							try {
+								img = ImageIO.read(new File("img/source.png"));
+								ImageIcon  icon = new ImageIcon(img); 
+								JLabel picLabel = new JLabel(icon);								 
+								GraphicsArea.tile[i][j].add(picLabel);
+								GraphicsArea.tile[i][j].validate();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
 					}
 				}
@@ -75,7 +95,17 @@ public class GeometryDraw implements Observer {
 				for(int i = 0; i < geo.getTiles().size(); i++) {
 					for(int j = 0; j < geo.getTiles().get(i).size(); j++) {
 						if(geo.getTiles().get(i).get(j).getPos().getX() == moun.getPos().getX() && geo.getTiles().get(i).get(j).getPos().getY() == moun.getPos().getY()) {
-							GraphicsArea.tile[i][j].setBackground(new Color(255, 0, 0));
+							BufferedImage img;
+							try {
+								img = ImageIO.read(new File("img/mountain.png"));
+								ImageIcon  icon = new ImageIcon(img); 
+								JLabel picLabel = new JLabel(icon);								 
+								GraphicsArea.tile[i][j].add(picLabel);
+								GraphicsArea.tile[i][j].validate();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
 					}
 				}
