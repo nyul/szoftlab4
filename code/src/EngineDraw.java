97d36a547ad90c9b61a554ad740d23e4d42fc0e0
FairyO <odett.trk@gmail.com>
2014-05-13 13:42:42 +0200

    komment v1.1

--- code/src/EngineDraw.java
+++ code/src/EngineDraw.java
@@ -3,9 +3,16 @@ import java.util.Observer;
 
 /**
  * A gyozelem es vereseg felugro ablakok miatt van szukseg erre az osztalyra
+ * Engine defeat() es victory() metodusainak meghivodasakor ertesitjuk az Obsevert,
+ * vagyis ezt az osztalyt, hogy allapotvaltozas tortent, igy hozzuk a grafikus feluletet
+ * osszhangba, az adott allapottal
  */
 public class EngineDraw implements Observer {
-	
+	/**
+	 * Engine notifyObservers() metodusanak hatasara hivokdik meg a fuggveny.
+	 * Attol fuggoen, hogy milyen bemeneti parametert kapott a meghivasakor
+	 * hivodik meg a flageket allito metodusok.
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
@@ -19,15 +26,23 @@ public class EngineDraw implements Observer {
 			updateStatus();
 	    }
 	}
-	
+	/**
+	 * Flag segitsegevel allitjuk be, hogy a grafikus feluleten, majd
+	 * melyik informacios uzenet jelenjen meg. Gyozelem eseten erteke a flagnek false.
+	 */
 	public void updateVictory(Observable o, Object arg) {
 		GraphicsArea.end = false;
 	}
-	
+	/**
+	 * Flag segitsegevel allitjuk be, hogy a grafikus feluleten, majd
+	 * melyik informacios uzenet jelenjen meg. Vereseg eseten az erteke a flagnek true.
+	 */
 	public void updateDefeat(Observable o, Object arg) {
 		GraphicsArea.end = true;
 	}
-	
+	/**
+	 * Ez a fuggveny hivja meg a tenylegesen kirajzolo fuggvenyt.
+	 */
 	public void updateStatus() {
 		GraphicsArea.endGameMessage();
 	}
