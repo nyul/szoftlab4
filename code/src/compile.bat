031b57aec123b3b26a43f474b04a49b0f6c82f8d
vcsontos <csontos.valentin@gmail.com>
2014-05-13 14:56:43 +0200

    compile.bat and run.bat refresh

--- code/src/compile.bat
+++ code/src/compile.bat
@@ -1,8 +1,13 @@
 javac Dwarf.java
 javac Elf.java
+javac EnemyDraw.java
 javac Engine.java
+javac EngineDraw.java
 javac Fellowship.java
+javac FellowshipDraw.java
 javac Geometry.java
+javac GeometryDraw.java
+javac GraphicsArea.java
 javac Human.java
 javac Hobbit.java
 javac Main.java
@@ -10,9 +15,12 @@ javac MagicRock.java
 javac Mountain.java
 javac Obstacle.java
 javac Player.java
+javac PlayerDraw.java
 javac PlayingArea.java
 javac Position.java
 javac Road.java
 javac Source.java
 javac Tile.java
 javac Tower.java
+javac TowerDraw.java
+javac Writer.java
--- code/src/run.bat
+++ code/src/run.bat
@@ -1,1 +1,1 @@
-java Main
+java GraphicsArea
